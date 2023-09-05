000cc38600aef9f63362d34182ead78a566f5d55
aldoyh <hd@doy.tech>
2023-09-06 00:06:41 +0300

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     starlight({
       title: 'My Docs',
       social: {
-        github: 'https://github.com/aldoyh/starlight',
+        github: 'https://github.com/aldoyh/github-ahvgkt',
       },
       sidebar: [
         {
